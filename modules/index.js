b134f02afd30ff9d528f1d1fe1f3db33124ef6bd
Aziz-Taufique <taufiqueaziz88@gmil.com>
2024-03-12 23:32:48 +0530

    first commit

---
+++ modules/index.js
@@ -0,0 +1,10 @@
+const math = require("./math"); // checking in current directory
+
+// math => gives an objext
+
+// const math = require("fs");   ==>  Checking the existing directory in node
+// const math = (require = "https");  ==> Checkig the existing directory in node
+
+console.log(math); // it gives an object
+
+console.log(`add vlaue is ${math.addVal(3, 5)} and sub val is ${(5, 3)}`);
---
+++ modules/math.js
@@ -0,0 +1,9 @@
+function addVal(a, b) {
+  return a + b;
+}
+
+function subVal(b, c) {
+  return b + c;
+}
+
+module.exports = { addVal, subVal };
