c5874dff2ba89def8dc10ab3ad1dd6486b207bed
Aziz-Taufique <taufiqueaziz88@gmil.com>
2024-03-13 15:56:59 +0530

    http method

--- server/index.js
+++ server/index.js
@@ -4,13 +4,13 @@ const url = require("url");
 
 const myServer = http.createServer((req, res) => {
   if (req.url === "/favicon.ico") return res.end();
-  const log = `${Date.now()}: ${req.url} New Req received\n`;
+  const log = `${Date.now()}: ${req.method} ${req.url} New Req received\n`;
   const myUrl = url.parse(req.url, true);
   console.log(myUrl);
   fs.appendFile("file.txt", log, (err, data) => {
     switch (myUrl.pathname) {
       case "/":
-        res.end("Home Page");
+        if (req.method === "GET") res.end("Home Page");
         break;
       case "/about":
         const username = myUrl.query.myname;
@@ -19,6 +19,12 @@ const myServer = http.createServer((req, res) => {
       case "/search":
         const search = myUrl.query.search_query;
         res.end("This is your result for f" + search);
+      case "/signup":
+        if (req.method === "GET") res.end("This is a signup form");
+        else if (req.method === "POST") {
+          // DB quary...
+          res.end("success");
+        }
       default:
         res.end("404 Not found");
     }
